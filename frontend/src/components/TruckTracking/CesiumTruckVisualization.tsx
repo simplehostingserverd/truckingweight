b615c9e559c4fe91c7182e20d4c647943bfcdde6
simplehostingserverd <simplehostingserverd@yahoo.com>
2025-05-10 09:01:30 -0500

    Fix Cesium import error and update UI/UX with modern design system

--- frontend/src/components/TruckTracking/CesiumTruckVisualization.tsx
+++ frontend/src/components/TruckTracking/CesiumTruckVisualization.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useRef, useState } from 'react';
 import * as Cesium from 'cesium';
-import { createWorldTerrain } from '@cesium/engine';
 import 'cesium/Build/Cesium/Widgets/widgets.css';
 
 interface RoutePoint {
@@ -40,7 +39,7 @@ export default function CesiumTruckVisualization({
 
       // Create Cesium viewer
       const cesiumViewer = new Cesium.Viewer(cesiumContainer.current, {
-        terrainProvider: createWorldTerrain(),
+        terrainProvider: Cesium.createWorldTerrain(),
         imageryProvider: new Cesium.IonImageryProvider({
           assetId: 3, // Sentinel-2 imagery
         }),
